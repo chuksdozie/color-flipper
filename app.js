9b653ada88149b17ef76d866c6e67534947da53b
Chuks dozie <chuksdozie48@gmail.com>
2021-04-23 15:31:51 +0100

    done with the exo-skeleton

--- app.js
+++ app.js
@@ -12,16 +12,36 @@
 
 // btn.addEventListener("click", nowch);
 
-let color = ["red", "yellow", "green","purple", "white", "whitesmoke", "orange"];
+let color = ["A","B","C","D","E","F","0","1","2","3","4","5","6","7","8","9"];
 let mycolor = document.getElementById("colorid");
-let btn = document.getElementById("btn");
-
+let manbtn = document.getElementById("manbtn");
+let autbtn = document.getElementById("autbtn");
+let stopbtn = document.getElementById("stopbtn");
 
 
 function nowch(){
-    let num = Math.floor(Math.random()*color.length)
-    document.body.style.backgroundColor = color[num];
-    mycolor.innerHTML = "Yellow";    
+    
+    let hexcolor = "#"
+    for (let i = 0; i<6; i++){
+        let num = Math.floor(Math.random()*color.length)
+        hexcolor += color[num];
+    }
+    console.log(hexcolor);
+    document.body.style.backgroundColor = hexcolor
+    mycolor.innerHTML = hexcolor;
+}
+var myv;
+// let myvar = setInterval(nowch,200)
+function autnowch(){
+   myv = setInterval(nowch,200);
 }
 
-btn.addEventListener("click", nowch);
+function stopnowch(){
+    clearInterval(myv);
+    console.log("fish")
+}
+
+manbtn.addEventListener("click", nowch);
+autbtn.addEventListener("click", autnowch);
+stopbtn.addEventListener("click", stopnowch);
+// setInterval(nowch,3000)
